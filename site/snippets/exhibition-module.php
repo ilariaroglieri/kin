8afa1175ac49d035d8ae7ad78537b023cd88f758
Ilaria Roglieri <ilariaroglieri@gmail.com>
2024-01-17 17:55:51 +0100

    building homepage

--- site/snippets/exhibition-module.php
+++ site/snippets/exhibition-module.php
@@ -1,29 +1,19 @@
-<?php 	
-	$exhibitions = page('exhibitions')->children()->listed(); 
+<?php 
+	$starting_year = $exhibition->starting_date()->toDate('Y');
+	$ending_year = $exhibition->starting_date()->toDate('Y');
+	$start = $starting_year == $ending_year ? $exhibition->starting_date()->toDate('d m') : $exhibition->starting_date()->toDate('d m Y');
+	$end = $exhibition->ending_date()->toDate('d m Y');
+?>
 
-  if ($exhibitions->count() > 0) { ?>
-  	<div class="d-flex flex-row">
-		  <?php foreach($exhibitions as $exhibition) { ?>
-		  	<?php 
-			  	$starting_year = $exhibition->starting_date()->toDate('Y');
-			  	$ending_year = $exhibition->starting_date()->toDate('Y');
-			  	$start = $starting_year == $ending_year ? $exhibition->starting_date()->toDate('d m') : $exhibition->starting_date()->toDate('d m Y');
-			  	$end = $exhibition->ending_date()->toDate('d m Y');
-		  	?>
+<div data-id="<?= $exhibition->uid() ?>" class="exhibition d-one-third t-half m-whole">
+	<a class="overall" href="<?= $exhibition->url() ?>"></a>
 
-	      <div data-id="<?= $exhibition->uid() ?>" class="exhibition d-one-third t-half m-whole">
-	      	<a class="overall" href="<?= $exhibition->url() ?>"></a>
+	<?php if ($image = $exhibition->c_image()->toFile()) { ?>
+		<div class="cover_img">
+			<img src="<?= $image->url(); ?>" />
+		</div>
+	<?php } ?>
 
-	      	<?php if ($image = $exhibition->c_image()->toFile()) { ?>
-						<div class="cover_img">
-							<img src="<?= $image->url(); ?>" />
-						</div>
-					<?php } ?>
-
-	      	<h3 class="s-small uppercase spacing-t-2"><?= $start ?> - <?= $end ?></h3>
-	      	<h2 class="s-medium bold italic spacing-t-half"><?= $exhibition->title()->html() ?></h2>
-	      </div>
-
-      <?php } ?>
-    </div>
-  <?php } ?>
+	<h3 class="s-small uppercase spacing-t-2"><?= $start ?> - <?= $end ?></h3>
+	<h2 class="s-medium bold italic spacing-t-half"><?= $exhibition->title()->html() ?></h2>
+</div>
--- site/templates/home.php
+++ site/templates/home.php
@@ -7,5 +7,32 @@
 			</div>
 		</div>
 
-		<?php snippet('exhibition-module') ?>
+		<?php 	
+		$exhibitions = page('exhibitions')->children()->listed(); 
+		$t = date("d-m-Y"); 
+		$today = new DateTime($t);
+
+		// just nu
+	  if ($exhibitions->count() > 0) { ?>
+	  	<div class="d-flex flex-row">
+	  		<div class="d-whole">
+	  			<h4 class="uppercase spacing t-2 spacing-b-2"><?php echo t('just-nu, Just nu'); ?></h4>
+	  		</div>
+	  	</div>
+	  	<div class="d-flex flex-row">
+			  <?php foreach($exhibitions as $exhibition): 
+					
+					$s = $exhibition->starting_date()->toDate('d-m-Y');
+					$startD = new DateTime($s);
+					$e = $exhibition->ending_date()->toDate('d-m-Y'); 
+					$endD = new DateTime($e);
+					?>
+
+			  	<?php if ($startD < $today && $today < $endD): ?>
+						<?php snippet('exhibition-module', array('exhibition' => $exhibition)) ?>
+					<?php endif; ?>
+
+	      <?php endforeach; ?>
+	    </div>
+	  <?php } ?>
 	</div>
