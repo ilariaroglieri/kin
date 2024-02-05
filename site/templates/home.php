37eb5576ac98972a5bb51c798d2bd97eb5e9b390
Ilaria Roglieri <ilariaroglieri@gmail.com>
2024-02-05 16:04:50 +0100

    lets begin frontend

--- site/templates/home.php
+++ site/templates/home.php
@@ -13,41 +13,31 @@
 			</div>
 		</div>
 
-
-
 		<!-- just nu -->
 	  <?php 
 	  	$home_events =  $page->related()->toPages();
 	  	if ( $home_events->count() > 0): 
 	  ?>
+
 	  	<div class="d-flex flex-row">
 	  		<div class="d-whole spacing-t-2">
-	  			<h4 class="uppercase spacing-b-2">Just nu<?php echo t('just-nu, Just nu'); ?></h4>
+	  			<h4 class="uppercase spacing-b-2">Pågår just nu<?php echo t('just-nu, Just nu'); ?></h4>
 	  		</div>
 	  	</div>
 	  	<div class="d-flex flex-row m-column">
-			  <?php foreach($home_events as $home_event): ?>
-			  	<?php if (!($event->starting_date()->isEmpty()) && !($event->ending_date()->isEmpty()) ):
-						
-							$s = $event->starting_date()->toDate('d-m-Y');
-							$startD = new DateTime($s);
-							$e = $event->ending_date()->toDate('d-m-Y'); 
-							$endD = new DateTime($e);
-						?>
-
-						  <?php if ($startD < $today && $today < $endD): ?>
-								<?php snippet('event-module', array('event' => $event)) ?>
-							<?php endif; ?>
-
-						<?php endif;?>
-				<?php endforeach ?>
+			  <?php foreach($home_events as $event): 
+			  	snippet('event-module', array('event' => $event));
+				endforeach; ?>
 	    </div>
+
 	  <?php else: ?>
+
 	  	<div class="d-flex flex-row">
 	  		<div class="d-whole spacing-t-2">
 	  			<h4 class="spacing-b-2">Nothing to display in home.</h4>
 	  		</div>
 	  	</div>
+	  	
 	  <?php endif; ?>
 
 	</div>
