92f613022a00022b2d3d0bb0bd9e3247c024bea0
Ilaria Roglieri <ilariaroglieri@gmail.com>
2025-05-27 13:25:58 +0200

    markup almost done

---
+++ site/snippets/catalogue-module.php
@@ -0,0 +1,17 @@
+<div data-id="<?= $catalogue->uid() ?>" class="catalogue d-one-fifth t-half spacing-b-2">
+
+	<?php if($pdf = $catalogue->pdf()->toFile()): ?>
+		<a class="overall" href="<?= $pdf->url(); ?>" alt="<?= $catalogue->title()->html()?>"><?= $catalogue->title()->html()?></a>
+	<?php endif; ?>
+
+	<?php if ($image = $catalogue->c_image()->toFile()): 
+		$cropped = $image->crop(951, 1143); ?>
+		<div class="cover_img" role="img" style="background: url('<?= $cropped->url(); ?>') no-repeat center center;">
+		</div>
+	<?php endif; ?>
+
+	<div class="catalogue-info">
+		<h2 class="s-small-2 spacing-t-2 spacing-b-half"><?= $catalogue->title()->html() ?></h2>
+		<?php snippet('catalogue-date', array('catalogue' => $catalogue)) ?>
+	</div>
+</div>
--- site/templates/pdfarchive.php
+++ site/templates/pdfarchive.php
@@ -1,93 +1,28 @@
 <?php snippet('header') ?>
 
-	<?php 	
-		$t = date("d-m-Y"); 
-		$today = new DateTime($t);
-
-		$archiveTime = $page->time();
-
-		$catfield = page('events')->children()->first()->blueprint()->field('cat');
-		$cats = $catfield['options']; 
-
-
-		$events = page('events')->children()->listed()->filterBy('eventStateInTime', $archiveTime)->filterBy('type', '!=', 'news'); // category news is hidden
-
-		if ($archiveTime == 'ongoing' || $archiveTime == 'future'):
-			$orderedEvents = $events->sortBy('starting_date','ASC'); 
-		else: 
-			$orderedEvents = $events->sortBy('starting_date','DESC'); 
-		endif;
-	?>
 	<div class="content" role="main">
 
-  	<div class="d-flex flex-row">
+		<div class="d-flex flex-row">
   		<div class="d-whole">
-  			<h2 class="s-big bold spacing-t-2"><?= $page->title() ?></h2>
+  			<h2 class="s-big bold spacing-t-2 spacing-b-4">Arkiv</h2>
   		</div>
   	</div>
 
+  	<div class="d-flex flex-row">
+			<div class="d-ten-twelfth t-whole">
+				<p><?= $site->archivetext()->kirbytext() ?></p>
+			</div>
+		</div>
 
-  	<?php if (!($archiveTime == 'ended')):
-			
-			$allcatsarray = [];
-			foreach($cats as $cat => $key):
-				$catname = $key[$kirby->language()->code()];
-				$catarray = [];
+  	<?php $catalogues = page('pdf-archive')->children()->listed(); ?>
 
-				foreach($events as $event): 
-					if ( $event->cat()->value() ==  $cat ):
-					$catarray[] = $event;
-					endif;
-				endforeach;
 
-				if (!empty($catarray)): 
-					$allcatsarray[] = $catarray;
-				endif; 
+  	<div class="d-flex flex-row wrap m-column spacing-t-3">
+		  <?php foreach($catalogues as $catalogue): 
+		  	snippet('catalogue-module', array('catalogue' => $catalogue));
+			endforeach; ?>
+    </div>
 
-				if (!empty($catarray)):?>
-					<div class="d-flex flex-row">
-			  		<div class="d-whole">
-			  			<h3 class="cat-title s-regular spacing-t-2 spacing-b-1"><?= $catname; ?></h3>
-			  		</div>
-			  	</div>
-
-			  	<div class="events-row d-flex flex-row d-column">
-					  <?php foreach($orderedEvents as $event): 
-							
-							if ( $event->cat()->value() == $cat ):
-								snippet('event-module-list', array('event' => $event));
-							endif;?>
-
-			      <?php endforeach; ?>
-			    </div>
-				<?php endif; ?>
-			<?php endforeach; ?>
-
-			<?php if (empty($allcatsarray)): ?>
-				<div class="d-flex flex-row">
-		  		<div class="d-whole">
-		  			<h3 class="cat-title s-regular spacing-t-2 spacing-b-1"><?= t('no-events'); ?></h3>
-		  		</div>
-		  	</div>
-		  <?php endif; ?>
-
-		<!-- past archive has pagination and no categories division -->
-		<?php else: ?>
-
-			<?php 
-				$list = $orderedEvents->paginate(20);
-				snippet('pagination', array('list' => $list)); ?>
-
-			<div class="events-row d-flex flex-row d-column spacing-t-2">
-			  <?php foreach($orderedEvents->paginate(20) as $event): 
-					
-					snippet('event-module-list', array('event' => $event));
-
-	      endforeach; ?>
-	    </div>
-
-	    <?php snippet('pagination', array('list' => $list)); ?>
-		<?php endif; ?>
 
 	</div>
 
